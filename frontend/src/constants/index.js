09239a50277f572d9bea3ed6789686221abebb1c
Andrew Dickinson <Andrew-Dickinson@users.noreply.github.com>
2023-04-30 16:20:32 -0400

    Allow overriding API endpoint via env var

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,5 @@
-const API_BASE = "/api";
-// const API_BASE = "http://localhost:8000/api";
+const API_BASE = process.env.REACT_APP_API_URL_OVERRIDE
+  ? process.env.REACT_APP_API_URL_OVERRIDE
+  : "/api";
 
 export const NEIGHBORS_DATA_URL = API_BASE + "/neighbors/";
