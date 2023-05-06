50f75463f5db4ae6a915d9657b58bbe694d9f6bd
Andrew Dickinson <Andrew-Dickinson@users.noreply.github.com>
2023-05-06 00:56:31 -0400

    Fix: commit hash doesn't build into docker container

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -5,5 +5,5 @@ set -xe
 export GIT_COMMIT_HASH=$(git rev-parse HEAD)
 echo "Building commit hash: $GIT_COMMIT_HASH into frontend container image"
 
-docker build --tag node-explorer-frontend explorer-frontend/
+docker build --tag node-explorer-frontend --build-arg GIT_COMMIT_HASH explorer-frontend/
 docker build --tag node-explorer explorer-backend/
