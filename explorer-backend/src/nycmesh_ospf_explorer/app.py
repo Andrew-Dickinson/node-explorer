8a2c62a9512d8b446288c158a7b96cfc41a9c16d
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2025-08-01 23:33:15 -0400

    Fix: time travel mode race condition

--- explorer-backend/src/nycmesh_ospf_explorer/app.py
+++ explorer-backend/src/nycmesh_ospf_explorer/app.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 CORS(app)
 
 if "FLASK_ENV" in os.environ:
-    graph = OSPFGraph()
-elif os.environ.get("DEBUG") == "true":
+    global_graph = OSPFGraph()
+if os.environ.get("DEBUG") == "true":
     from test_graph import (
         TEST_NINE_NODE_GRAPH,
         TEST_NINE_NODE_GRAPH_WITH_ASYMMETRIC_COSTS,
         TEST_REAL_GRAPH_SEP_2023,
     )
 
-    graph = OSPFGraph(load_data=False)
-    graph.update_link_data(TEST_NINE_NODE_GRAPH)
+    global_graph = OSPFGraph(load_data=False)
+    global_graph.update_link_data(TEST_NINE_NODE_GRAPH)
     # graph.update_link_data(TEST_NINE_NODE_GRAPH_WITH_ASYMMETRIC_COSTS)
     # graph.update_link_data(TEST_REAL_GRAPH_SEP_2023)
 
@@ -40,30 +40,32 @@ def get_neighbors(router_id):
     neighbor_depth = int(request.args.get("searchDistance", 1))
     include_egress = request.args.get("includeEgress", "false") == "true"
 
-    if request.args.get("timestamp"):
-        graph.update_from_timestamp(
-            datetime.datetime.fromtimestamp(int(request.args.get("timestamp")))
-        )
-    else:
-        graph.update_if_needed()
+    request_graph = get_request_graph()
 
-    if not graph.contains_router(router_id):
+    if not request_graph.contains_router(router_id):
         return str(f"Couldn't find router with ID: {router_id}"), 404
 
     return {
-        **graph.get_neighbors_dict(router_id, neighbor_depth, include_egress=include_egress),
-        "updated": int(graph.last_updated.timestamp()),
+        **request_graph.get_neighbors_dict(router_id, neighbor_depth, include_egress=include_egress),
+        "updated": int(request_graph.last_updated.timestamp()),
     }
 
 
-@app.route("/simulate-outage", methods=["GET"])
-def simulate_outage():
+def get_request_graph():
+    request_graph = global_graph
     if request.args.get("timestamp"):
-        graph.update_from_timestamp(
+        request_graph = OSPFGraph()
+        request_graph.update_from_timestamp(
             datetime.datetime.fromtimestamp(int(request.args.get("timestamp")))
         )
     else:
-        graph.update_if_needed()
+        request_graph.update_if_needed()
+
+    return request_graph
+
+@app.route("/simulate-outage", methods=["GET"])
+def simulate_outage():
+    request_graph = get_request_graph()
 
     nodes_param = request.args.get("nodes", "")
     edges_param = request.args.get("edges", "")
@@ -75,48 +77,43 @@ def simulate_outage():
         return str("Must provide at least one of: edges, nodes"), 400
 
     for node in nodes:
-        if not graph.contains_router(node):
+        if not request_graph.contains_router(node):
             return str(f"Couldn't find router with ID: {node}"), 400
 
     for edge in edges:
         for router_id in edge:
-            if not graph.contains_router(router_id):
+            if not request_graph.contains_router(router_id):
                 return str(f"Couldn't find router with ID: {router_id}"), 400
 
         if edge[0] == edge[1]:
             return str(f"Self loops don't exist"), 400
 
-        graph_edges = graph.get_edges_for_node_pair(edge[0], edge[1])
+        graph_edges = request_graph.get_edges_for_node_pair(edge[0], edge[1])
         if len(graph_edges) == 0:
             return str(f"Couldn't find edge connecting routers: {edge[0]} & {edge[1]}"), 400
 
     return {
-        **graph.simulate_outage(nodes, edges),
-        "updated": int(graph.last_updated.timestamp()),
+        **request_graph.simulate_outage(nodes, edges),
+        "updated": int(request_graph.last_updated.timestamp()),
     }
 
 
 @app.route("/edges/<router1_id>/<router2_id>", methods=["GET"])
 def get_edges(router1_id, router2_id):
-    if request.args.get("timestamp"):
-        graph.update_from_timestamp(
-            datetime.datetime.fromtimestamp(int(request.args.get("timestamp")))
-        )
-    else:
-        graph.update_if_needed()
+    request_graph = get_request_graph()
 
     for router_id in [router1_id, router2_id]:
-        if not graph.contains_router(router_id):
+        if not request_graph.contains_router(router_id):
             return str(f"Couldn't find router with ID: {router_id}"), 404
 
     if router1_id == router2_id:
         return str(f"Self loops don't exist"), 404
 
-    edges = graph.get_edges_for_node_pair(router1_id, router2_id)
+    edges = request_graph.get_edges_for_node_pair(router1_id, router2_id)
     if len(edges) == 0:
         return str(f"Couldn't find edge connecting routers: {router1_id} & {router2_id}"), 404
 
     return {
         "edges": edges,
-        "updated": int(graph.last_updated.timestamp()),
+        "updated": int(request_graph.last_updated.timestamp()),
     }
